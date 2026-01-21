27db9b8c0c075ed6ee8f2ec583430739f85ab635
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-01-22 00:21:28 +0530

    Render and circle creation functionality implemented

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ const state = {
 let rectangle = document.querySelector('#rectangle');
 let circle = document.querySelector('#circle');
 let imageInput = document.querySelector('#image-input');
+let imageBtn = document.querySelector('#image');
+let textBtn = document.querySelector('#text');
 let deleteBtn = document.querySelector('#delete');
 let canvas = document.getElementById('canvas');
 
-let textBtn = document.querySelector('#text');
 
+imageBtn.addEventListener('click', () => {
+    imageInput.click();
+});
 
 function generateId() {
   return 'id_' + Date.now();
@@ -41,3 +45,51 @@ function createRectangle() {
   state.elements.push(element);
   renderElement(element);
 }
+
+function createCircle() {
+  const width = 120;
+  const height = 120;
+
+  const x = (canvas.clientWidth - width) / 2;
+  const y = (canvas.clientHeight - height) / 2;
+
+  const element = {
+    id: generateId(),
+    type: 'circle',
+    x: x,
+    y: y,
+    width: width,
+    height: height,
+    rotation: 0,
+    styles: {
+      backgroundColor: '',
+      borderRadius: '',
+      text: '',
+      imageSrc: ''
+    },
+    zIndex: state.elements.length + 1
+  };
+
+  state.elements.push(element);
+  renderElement(element);
+}
+
+function renderElement(el) {
+  const div = document.createElement('div');
+  const resizeHandle = document.createElement('div');
+  div.dataset.id = el.id;
+  div.classList.add(el.type);
+  resizeHandle.classList.add('resize');
+  div.appendChild(resizeHandle);
+
+  div.style.left = el.x + 'px';
+  div.style.top = el.y + 'px';
+  div.style.width = el.width + 'px';
+  div.style.height = el.height + 'px';
+  div.style.zIndex = el.zIndex;
+
+  canvas.appendChild(div);
+}
+
+rectangle.addEventListener('click', createRectangle);
+circle.addEventListener('click', createCircle);
